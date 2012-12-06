9c24847a25f27b7ba43f78cf388cf464a6de68db
Lucas van Lierop <lucas@vanlierop.org>
2012-12-06 16:48:06 +0100

    Disabled file renaming for now

--- Directory.php
+++ Directory.php
@@ -43,6 +43,9 @@ class MusicOrganizer_Directory extends DirectoryIterator
     }
 
     protected function _convertFile($file) {
+        return false;
+
+
         try {
            $file = MusicOrganizer_File::factory($file->getPathName());
         } catch (Exception $ex) {
